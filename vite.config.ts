ba1cdb5a53ccbc3c518a7942303fc45b3d646b50
David0403 <thanhtuvo135@gmail.com>
2023-12-18 02:46:05 +0700

    Add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import glsl from "vite-plugin-glsl";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite'
+import glsl from 'vite-plugin-glsl'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [glsl(), dts()],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      formats: ["es"],
-      entry: "src/index.ts",
-      fileName: "index",
-    },
-  },
-});
+	plugins: [glsl(), dts()],
+	build: {
+		copyPublicDir: false,
+		lib: {
+			formats: ['es'],
+			entry: 'src/index.ts',
+			fileName: 'index'
+		}
+	}
+})
